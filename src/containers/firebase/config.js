097eec61076b3b1ff1c40897a06c20c8d03cd90d
KoheiAsano <komerumeru@i.softbank.jp>
2019-01-24 20:22:26 +0900

    fix firebase path

--- src/containers/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC8Vh5FWgLsbPjrinFxWCgkip1TXres2fs",
-  authDomain: "r-r-f-todo.firebaseapp.com",
-  databaseURL: "https://r-r-f-todo.firebaseio.com",
-  projectId: "r-r-f-todo",
-  storageBucket: "r-r-f-todo.appspot.com",
-  messagingSenderId: "544806072099"
-};
